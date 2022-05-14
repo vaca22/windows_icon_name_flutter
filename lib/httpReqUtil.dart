ec0c079ecfb1775ae3ebd0a3452b0aed5ac8e5fa
vaca22 <wsmslgh@gmail.com>
2022-05-14 15:01:44 +0800

    asdf54hkjfd45kj845dffgdhdsfgdfgsdfdf

--- lib/httpReqUtil.dart
+++ lib/httpReqUtil.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart'tp;
 
 class HttpReqUtil {
   Future<http.Response> postFile(File f) async {
-    var url = Uri.parse('http://192.168.6.106/upload/good.mp3');
+    String name = basename(f.path);
+    var url = Uri.parse('http://192.168.6.106/upload/' + name);
     var response = await http.post(url, body: f.readAsBytesSync());
     return response;
   }
